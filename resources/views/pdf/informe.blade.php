5241f17a4ad23ab1fc9c2eb66af29bd76d7ad25d
josevasquezramos <jmvr16092003@gmail.com>
2025-07-19 12:18:22 -0500

    Cambio de orden titulo informe

--- resources/views/pdf/informe.blade.php
+++ resources/views/pdf/informe.blade.php
@@ -1,7 +1,7 @@
 <x-pdf-layout title="Informe {{ $trabajo->codigo }}" code="{{ $trabajo->codigo }}" tipoReporte="INFORME TÉCNICO">
     <div class="mb-4">
-        <h2 class="bold text-center">{{ $titulo }}</h2>
         <h2 class="normal text-center">INFORME TÉCNICO</h2>
+        <h2 class="bold text-center">{{ $titulo }}</h2>
     </div>
 
     @foreach($informes as $informe)
